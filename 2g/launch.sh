255d7ee996d7547de5dc032a96d4f9681b9a0eb1
kasperekd <kasperekd@mail.ru>
2024-10-17 22:44:00 +0700

    added new config

--- 2g/launch.sh
+++ 2g/launch.sh
@@ -2,9 +2,9 @@
 
 pids=()
 CFG_PATH="$PWD/cfg"
-# USER_HOME="$HOME" # or... 
+USER_HOME="$HOME" # or... 
 # USER_HOME="/home/<YOUR USER>" 
-USER_HOME="/home/kasperekd" 
+# USER_HOME="/home/kasperekd" 
 
 echo $CFG_PATH
 start_program() {
@@ -20,7 +20,7 @@ start_program osmo-msc "$CFG_PATH/osmo-msc.cfg" &
 start_program osmo-bsc "$CFG_PATH/osmo-bsc.cfg" &
 
 sudo xterm -e python3 "$USER_HOME/2G/osmocom-bb/src/target/trx_toolkit/fake_trx.py" &
-start_program "osmo-bts-trx" "$CFG_PATH/osmo-bts.cfg" &
+start_program "osmo-bts-trx" "$CFG_PATH/osmo-bts-trx.cfg" &
 start_program "$USER_HOME/2G/osmocom-bb/src/host/trxcon/src/trxcon" &
 start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/misc/ccch_scan" -a ARFCN -i 127.0.0.1 &
 
