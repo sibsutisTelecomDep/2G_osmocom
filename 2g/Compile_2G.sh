08618987fb281aefb5de0d1f6c64aa36eb53ce0b
kasperekd <kasperekd@mail.ru>
2024-10-17 18:26:56 +0700

    compile improved

--- 2g/Compile_2G.sh
+++ 2g/Compile_2G.sh
@@ -1,46 +1,80 @@
 #!/bin/bash
 
-sudo apt-get install -y build-essential libtool libtalloc-dev libsctp-dev shtool autoconf automake git-core pkg-config make gcc gnutls-dev python2-minimal libusb-1.0.0-dev libmnl-dev libpcsclite-dev
-sudo apt-get install -y build-essential libtool libortp-dev dahdi-source libsctp-dev shtool autoconf automake git-core pkg-config make gcc 
-sudo apt-get install -y libsqlite3-dev libc-ares-dev libtalloc-dev libpcsclite-dev libusb-1.0-0-dev libgnutls28-dev libmnl-dev xfonts-base
-osmo_src=$HOME/2G
-#Компиляция libosmocore.
-mkdir $osmo_src
-cd $osmo_src
+# Путь для установки
+install_path="/home/kasperekd/2G"
+
+update_system() {
+    echo "Обновление списка пакетов и апгрейд системы..."
+    sudo apt-get update
+    read -p "Нажмите Enter для продолжения сборки"
+    sudo apt-get upgrade -y
+    read -p "Нажмите Enter для продолжения сборки"
+}
+
+# установка пакетов
+install_packages() {
+    echo "Установка необходимых пакетов..."
+    sudo apt-get install -y python3
+    read -p "Нажмите Enter для продолжения сборки"
+    sudo apt-get install -y build-essential libtool libtalloc-dev libsctp-dev shtool autoconf automake git-core pkg-config make gcc gnutls-dev libusb-1.0.0-dev libmnl-dev libpcsclite-dev \
+                            libortp-dev dahdi-source libsqlite3-dev libc-ares-dev libgnutls28-dev xfonts-base
+
+    read -p "Нажмите Enter для продолжения сборки"                         
+}
+
+# Функция для клонирования и сборки проекта
+build_project() {
+    local repo_url=$1
+    local repo_name=$2
+    local skip_check=$3 
+
+    echo "Сборка $repo_name..."
+    cd $install_path
+    git clone $repo_url
+    cd $repo_name
+    autoreconf -fi
+    ./configure
+    # ./configure --prefix=$install_path
+    make -j$(nproc)
+
+    if [ "$skip_check" != "true" ]; then
+        make check
+    else
+        echo "Пропуск make check для $repo_name"
+    fi
+
+    read -p "Нажмите Enter для продолжения сборки $repo_name"
+    sudo make install
+    sudo ldconfig
+}
+
+# Обновление системы
+update_system
+# Установка пакетов
+install_packages
+
+# Установка и сборка liburing 
+osmo_src=$install_path
+mkdir -p $osmo_src
+
 git clone https://github.com/axboe/liburing.git
 cd liburing
 ./configure
-make
-sudo make install
-read -p "libosmocore"
-array=(libosmocore libosmo-abis libosmo-netif libosmo-sccp libosmo-sigtran)
-for i in ${array[@]}
-do
-
-cd $osmo_src
-git clone https://gitea.osmocom.org/osmocom/$i.git
-cd $i
-autoreconf -fi
-./configure
 make -j$(nproc)
-make check
-read -p "$i"
 sudo make install
-sudo ldconfig
+
+# Сборка библиотеки libosmocore и связанных с ней библиотек
+declare -a osmocom_libs=("libosmocore" "libosmo-abis" "libosmo-netif" "libosmo-sccp" "libosmo-sigtran")
+
+for lib in "${osmocom_libs[@]}"; do
+    build_project "https://gitea.osmocom.org/osmocom/$lib.git" "$lib" "false"
 done
 
-array=(libsmpp34 osmo-mgw libasn1c osmo-iuh osmo-hlr osmo-msc osmo-ggsn osmo-sgsn osmo-sgsn osmo-bsc osmo-bts)
-for i in ${array[@]}
-do
+# Сборка библиотек и компонентов мобильной инфраструктуры
+declare -a osmocom_components=("libsmpp34" "osmo-mgw" "libasn1c" "osmo-iuh" "osmo-hlr" "osmo-msc" "osmo-ggsn" "osmo-sgsn" "osmo-bsc" "osmo-bts" "osmo-bb")
 
-cd $osmo_src
-git clone https://gitea.osmocom.org/cellular-infrastructure/$i.git
-cd $i
-autoreconf -fi
-./configure
-make -j$(nproc)
-make check
-read -p "$i"
-sudo make install
-sudo ldconfig
+for component in "${osmocom_components[@]}"; do
+    build_project "https://gitea.osmocom.org/cellular-infrastructure/$component.git" "$component" "false"
 done
+
+echo "Скрипт завершен!"
