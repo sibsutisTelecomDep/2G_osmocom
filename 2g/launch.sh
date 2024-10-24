b15f13670608066d444c650cc318adc13c5c3534
kasperekd <kasperekd@mail.ru>
2024-10-24 20:39:01 +0700

    nex itter

--- 2g/launch.sh
+++ 2g/launch.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 
 pids=()
-CFG_PATH="$PWD/cfg"
-USER_HOME="$HOME" # or... 
+CFG_PATH="$PWD/cfg1"
+# USER_HOME="$HOME" # or... 
 # USER_HOME="/home/<YOUR USER>" 
-# USER_HOME="/home/kasperekd" 
+USER_HOME="/home/kasperekd" 
 
 echo $CFG_PATH
 start_program() {
-    sudo xterm -e "$1" -c "$2" &
+    sudo xterm -hold -e "$1" -c "$2" &
     pids+=($!)
     sleep 0.2
 }
 
-start_program osmo-mgw "$CFG_PATH/osmo-mgw.cfg" &
-start_program osmo-hlr "$CFG_PATH/osmo-hlr.cfg" &
-start_program osmo-stp "$CFG_PATH/osmo-stp.cfg" &
-start_program osmo-msc "$CFG_PATH/osmo-msc.cfg" &
-start_program osmo-bsc "$CFG_PATH/osmo-bsc.cfg" &
+sudo killall osmo-msc osmo-bsc osmo-mgw osmo-hlr osmo-stp mobile osmo-bts-trx fake_trx.py trxcon ccch_scan xterm mobile 2>/dev/null
 
+start_program osmo-hlr "$CFG_PATH/osmo-hlr.cfg"
+start_program osmo-msc "$CFG_PATH/osmo-msc.cfg"
+start_program osmo-mgw "$CFG_PATH/osmo-mgw-for-msc.cfg"
+start_program osmo-mgw "$CFG_PATH/osmo-mgw-for-bsc.cfg"
+start_program osmo-stp "$CFG_PATH/osmo-stp.cfg"
+start_program osmo-bsc "$CFG_PATH/osmo-bsc.cfg"
+
+echo "Press Enter to continue..."
+read e
+
+# Запуск остальных компонентов
 sudo xterm -e python3 "$USER_HOME/2G/osmocom-bb/src/target/trx_toolkit/fake_trx.py" &
-start_program "osmo-bts-trx" "$CFG_PATH/osmo-bts-trx.cfg" &
+start_program "osmo-bts-trx" "./cfg/osmo-bts-trx.cfg" &
 start_program "$USER_HOME/2G/osmocom-bb/src/host/trxcon/src/trxcon" &
-start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/misc/ccch_scan" -a ARFCN -i 127.0.0.1 &
-
-start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "$CFG_PATH/default.cfg" &
+echo "Press Enter to continue..."
+read e
+# start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "$CFG_PATH/mobile2.cfg" &
+start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "$CFG_PATH/mobile.cfg" &
 
 echo "Press Enter to close all programs..."
 read enter_to_close
@@ -33,4 +41,24 @@ for pid in "${pids[@]}"; do
     sudo kill "$pid" 2>/dev/null
 done
 
-sudo killall osmo-msc osmo-bsc osmo-mgw osmo-hlr osmo-stp mobile osmo-bts-trx fake_trx.py trxcon xterm 2>/dev/null
+sudo killall osmo-msc osmo-bsc osmo-mgw osmo-hlr osmo-stp mobile osmo-bts-trx fake_trx.py trxcon ccch_scan xterm mobile 2>/dev/null
+
+
+
+# БОЕВОЙ РЕЗЕРВ
+# sudo xterm -e python3 "$USER_HOME/2G/osmocom-bb/src/target/trx_toolkit/fake_trx.py" &
+# start_program "osmo-bts-trx" "./cfg/osmo-bts-trx.cfg" &
+# # start_program "osmo-bts-virtual" "$CFG_PATH/osmo-bts-virtual.cfg" &
+# start_program "$USER_HOME/2G/osmocom-bb/src/host/trxcon/src/trxcon" &
+# # sudo xterm -hold -e $USER_HOME/2G/osmocom-bb/src/host/trxcon/src/trxcon --trx-port 5700 -d DAPP:DL1C:DSCH -s /tmp/osmocom_l2 &
+# # sudo xterm -hold -e $USER_HOME/2G/osmocom-bb/src/host/trxcon/src/trxcon -d DAPP:DL1C:DSCH -s /tmp/osmocom_l2 &
+# # start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/misc/ccch_scan" -a 774 -i 127.0.0.1 &
+# # sudo xterm -hold -e "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/misc/ccch_scan" -a ARFCN -i 127.0.0.1 &
+# # sudo xterm -hold -e "/home/kasperekd/2G/osmocom-bb/src/host/virt_phy/src/virtphy" &
+# echo "Press Enter to continue..."
+# read e
+# # start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "$CFG_PATH/mobile2.cfg" &
+# start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "$CFG_PATH/mobile.cfg" &
+# # start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "./cfg/mobile.cfg" &
+# # start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" "./cfg/default.cfg" &
+# # start_program "$USER_HOME/2G/osmocom-bb/src/host/layer23/src/mobile/mobile" -i 127.0.0.1 &
